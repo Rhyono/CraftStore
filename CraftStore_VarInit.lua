713b1d80912a1a00aacefd86ff29a080ba3394d8
Michael Dominik Görtz <dgoertz@posteo.de>
2023-07-30 00:31:04 +0200

    Update addon version number and ESO API version to latest live version

--- CraftStore_VarInit.lua
+++ CraftStore_VarInit.lua
@@ -4,7 +4,7 @@ CS.Debug = (GetWorldName() == "PTS" or GetDisplayName()=="@VladislavAksjonov")
 CS.Name = 'CraftStoreFixedAndImproved'
 CS.Title = 'CraftStore'
 CS.Author = 'AlphaLemming, BlackSwan, Rhyono'
-CS.Version = '2.86'
+CS.Version = '2.87'
 CS.Account = nil
 CS.Character = nil
 CS.Init = false
