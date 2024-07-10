51079701f0153e412e4ba1b175e7e9e8c1383b22
Rhyono <rhyono.dev@gmail.com>
2024-07-10 11:21:32 -0400

    Version

--- CraftStore_VarInit.lua
+++ CraftStore_VarInit.lua
@@ -4,7 +4,7 @@ CS.Debug = (GetWorldName() == "PTS" or GetDisplayName()=="@VladislavAksjonov")
 CS.Name = 'CraftStoreFixedAndImproved'
 CS.Title = 'CraftStore'
 CS.Author = 'AlphaLemming, BlackSwan, Rhyono, MuMuQ'
-CS.Version = '2.92'
+CS.Version = '2.93'
 CS.Account = nil
 CS.Character = nil
 CS.Init = false
